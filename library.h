f0b6e1411297864cc99379d44359256a45392e82
RMashkalev <rmashkalev@mail.ru>
2021-03-19 12:35:45 +0700

    add dir

--- library.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include "display_board.h"
-#include "move.h"
---
+++ scr/function/library.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdio.h>
+#include <function/display_board.h>
+#include <function/move.h>
